3144fa3260dc7c98df9c5921bc631d0c497fa9ed
psegynola <philipsegynola@gmail.com>
2026-08-04 16:33:11 -0600

    Add Terraform setup configuration

--- infrastructure/setup/main.tf
+++ infrastructure/setup/main.tf
@@ -1,29 +1,29 @@
-terraform{
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "5.23.0"
-        }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
     }
+  }
 
-    backend "s3" {
-        bucket="fdivine-receipe-app-tf-state"
-        key="tf-state-setup"
-        region="us-east-1"
-        encrypt=true
-        dynamodb_table="fdivine-receipe-app-tf-lock"
-    }
+  backend "s3" {
+    bucket         = "fdivine-recipe-app-tf-state"
+    key            = "tf-state-setup"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "fdivine-recipe-app-tf-lock"
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 
-    default_tags{
-        tags = {
-            Environment = "terraform.workspace"
-            Project = var.project
-            Contact = var.contact
-            ManageBy = "Terraform/setup"
-        }
+  default_tags {
+    tags = {
+      Environment = "terraform.workspace"
+      Project     = var.project
+      Contact     = var.contact
+      ManageBy    = "Terraform/setup"
     }
+  }
 }
--- infrastructure/setup/variables.tf
+++ infrastructure/setup/variables.tf
@@ -1,23 +1,23 @@
 variable "tf_state_bucket" {
-    description = "The name of the S3 bucket to store the Terraform state file."
-    type = string
-    default = "fdivine-receipe-app-tf-state"
+  description = "The name of the S3 bucket to store the Terraform state file."
+  type        = string
+  default     = "fdivine-recipe-app-tf-state"
 }
 
 variable "tf_state_lock_table" {
-    description = "The name of the DynamoDB table to use for state locking."
-    type = string
-    default = "fdivine-receipe-app-tf-lock"
+  description = "The name of the DynamoDB table to use for state locking."
+  type        = string
+  default     = "fdivine-recipe-app-tf-lock"
 }
 
 variable "project" {
-    description = "The name of the project."
-    type = string
-    default = "receipe-app-api"
+  description = "The name of the project."
+  type        = string
+  default     = "recipe-app-api"
 }
 
 variable "contact" {
-    description = "The contact information for the project."
-    type = string
-    default = "devops@fdivine.com"
+  description = "The contact information for the project."
+  type        = string
+  default     = "devops@fdivine.com"
 }
